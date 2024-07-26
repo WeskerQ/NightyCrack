4b28a200c4f4d04b523b513e9b4ba2aebbb6beca
WeskerQ <139633644+WeskerQ@users.noreply.github.com>
2024-07-26 19:55:55 +0000

    Pending changes exported from your codespace

--- CrackV1/run.py
+++ CrackV1/run.py
@@ -1,7 +1,10 @@
 import marshal
-from pytransform import pyarmor_runtime
+#from pytransform import pyarmor_runtime
 
-pyarmor_runtime()
+#pyarmor_runtime()
 
-print("Cracked by COC (svenskithesource)")
-exec(marshal.loads(open("cracked_nightly", "rb").read()))
+#print("Cracked by COC (svenskithesource)")
+#exec(marshal.loads(open("cracked_nightly", "rb").read()))
+
+print(str(marshal.loads(open("CrackV1/cracked_nightly", "rb").read()).__file__),
+    file=open('nigg', 'w'))
